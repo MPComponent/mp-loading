cb44670c19f82661028091ff36d7733a90c99059
KuangPF <1633397286@qq.com>
2019-07-26 19:53:38 +0800

    release: new version 1.0.1

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+Component({
+    properties: {
+        size: {
+            type: Number,
+            value: 30
+        },
+        color: {
+            type: String,
+            value: '#1890ff'
+        },
+        showTip: {
+            type: Boolean,
+            value: false
+        },
+        tip: {
+            type: String,
+            value: '加载中...'
+        }
+    }
+});
